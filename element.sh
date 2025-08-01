821bedfcf4ee065ce640b0fbc594648b5ec6bd65
Ter-er <tfgbem@gmail.com>
2025-08-01 19:23:59 -0400

    test: Test passed

--- element.sh
+++ element.sh
@@ -5,12 +5,13 @@ PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only --no
 
 echo -e "\nPlease provide an element as an arguement.\n"
 
-
+# Queries database using both tables: elements and properties
 ELEMENT_INFO=$($PSQL "SELECT elements.atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties ON elements.atomic_number = properties.atomic_number;")
 
+# Reads database values into variables line by line
 while IFS='|' read ATOMIC_NO NAME SYMBOL TYPE MASS MELTING_P BOILING_P 
 do
-  if [[ "$1" == "$ATOMIC_NO" ||  "$1" == "$SYMBOL" || "$1" == "$NAME" ]]
+  if [[ "$1" == "$ATOMIC_NO" ||  "$1" == "$SYMBOL" || "$1" == "$NAME" ]] # Checks if the first argument is valid and prints the information for the chosen element
   then
     echo "The element with atomic number $ATOMIC_NO is $NAME ("$SYMBOL"). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_P celsius and a boiling point of $BOILING_P celsius." 
     FOUND=1
@@ -19,7 +20,7 @@ do
 done <<< "$ELEMENT_INFO"
 
 
-if [[ -z $FOUND ]]
+if [[ -z $FOUND ]] # If the first argument is not valid
 then
   echo I could not find that element in the database.
 fi
