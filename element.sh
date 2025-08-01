a36bd51a6758a671e0f77a318bb4c1309ca29b20
Ter-er <tfgbem@gmail.com>
2025-08-01 19:11:11 -0400

    feat:Input argument done

--- element.sh
+++ element.sh
@@ -5,9 +5,22 @@ PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only --no
 
 echo -e "\nPlease provide an element as an arguement.\n"
 
-if [[ $1 = 1 ]]
+
+ELEMENT_INFO=$($PSQL "SELECT elements.atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties ON elements.atomic_number = properties.atomic_number;")
+
+while IFS='|' read ATOMIC_NO NAME SYMBOL TYPE MASS MELTING_P BOILING_P 
+do
+  if [[ "$1" == "$ATOMIC_NO" ||  "$1" == "$SYMBOL" || "$1" == "$NAME" ]]
+  then
+    echo "The element with atomic number $ATOMIC_NO is $NAME ("$SYMBOL"). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_P celsius and a boiling point of $BOILING_P celsius." 
+    FOUND=1
+    break
+  fi
+done <<< "$ELEMENT_INFO"
+
+
+if [[ -z $FOUND ]]
 then
-  ELEMENT_INFO=$($PSQL "SELECT elements.atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties ON elements.atomic_number = properties.atomic_number WHERE elements.atomic_number=1;")
-  IFS='|' read ATOMIC_NO NAME SYMBOL TYPE MASS MELTING_P BOILING_P <<< "$ELEMENT_INFO"
-  echo "The element with atomic number $ATOMIC_NO is $NAME ("$SYMBOL"). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_P celsius and a boiling point of $BOILING_P celsius." 
+  echo I could not find that element in the database.
 fi
+
